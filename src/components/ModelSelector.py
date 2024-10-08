81511a0b8328b3867c78b68f4fdfd865f94f6c9a
Shogo Ogami <sg.tao.so@gmail.com>
2024-10-09 08:01:42 +0900

    Fix:update ModelSelector component for modify order of model list

--- src/components/ModelSelector.py
+++ src/components/ModelSelector.py
@@ -8,13 +8,13 @@ class ModelSelector:
     def __init__(self):
         """Define the available models"""
         self.models = [
+            "llama-3.1-8b-instant",
+            "llama-3.1-70b-versatile",
+            "llama3-groq-8b-8192-tool-use-preview",
+            "llama3-groq-70b-8192-tool-use-preview",
             "gemma2-9b-it",
             "gemma-7b-it",
-            "llama3-groq-70b-8192-tool-use-preview",
-            "llama3-groq-8b-8192-tool-use-preview",
-            "llama-3.1-70b-versatile",
-            "llama-3.1-8b-instant",
-            "llama-3.2-1b-preview",  # shot token
+            "llama-3.2-1b-preview",  # short token
             "llama-3.2-3b-preview",
             "llama-3.2-11b-vision-preview",
             # "llama-3.2-90b-vision-preview", # NotFoundError yet.
@@ -22,7 +22,7 @@ class ModelSelector:
             # "llava-v1.5-7b-4096-preview", # BadRequestError at chat.
             "llama3-70b-8192",
             "llama3-8b-8192",
-            "mixtral-8x7b-32768",  # shot token
+            "mixtral-8x7b-32768",  # short token. why?
         ]
 
     def select(self):
